1a7ff7e4478b04bffe043f1d250109cd856d4aa7
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-11-29 18:16:54 -0800

    item pg updates & readme updates

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for
+
+# scraper for getting data
 # from bs4 import BeautifulSoup as bs
 # import requests
 
@@ -11,6 +13,8 @@ app = Flask(__name__)
 ############################################################
 # Test Data
 ############################################################
+
+# get data from redbuble
 # r = requests.get("https://www.redbubble.com/shop/ap/59785340")
 # soup = bs(r.content)
 # items = soup.find_all("a", attrs={"class": "styles__link--2sYi3"})
@@ -20,13 +24,13 @@ app = Flask(__name__)
 
 art_products = [
     {
-        "src": "data:image/jpg;base64,R0lGODdhFQAXAPAAANba3wAAACwAAAAAFQAXAAACFISPqcvtD6OctNqLs968+w+GolUAADs=",
+        "src": "https://ih1.redbubble.net/image.1751903044.6537/fp,840x830,black,off_white,box20,s,f8f8f8-pad,1000x1000,f8f8f8.jpg",
         "alt": "#",
         "title": "Example Product",
         "_id": 123456789
     },
     {
-        "src": "data:image/gif;base64,R0lGODdhFQAXAPAAANba3wAAACwAAAAAFQAXAAACFISPqcvtD6OctNqLs968+w+GolUAADs=",
+        "src": "https://ih1.redbubble.net/image.1751903044.6537/fp,840x830,black,off_white,box20,s,f8f8f8-pad,1000x1000,f8f8f8.jpg",
         "alt": "#",
         "title": "Example Product",
         "_id": 7653413
@@ -266,6 +270,21 @@ def item(item_id):
         "price": 499.49,
         "sale": True,
         "discount": 211.27,
+        "specifications": [
+            {
+                "header": "Expl Header",
+                "info": "Expl info"
+            },
+            {
+                "header": "Expl Header",
+                "info": "Expl info"
+            },
+            {
+                "header": "Expl Header",
+                "info": "Expl info"
+            }
+        ],
+        "art_products": art_products
     }
 
     if context["sale"]:
--- readme.md
+++ readme.md
@@ -5,14 +5,16 @@
 
 **Languages/Frameworks Used:**
 
-Python, JS, Flask, Jinga2, jQuery, HTML/CSS, Bootstrap
+Python, JS, Flask, Jinga2, jQuery, HTML/CSS, Bootstrap/Bootstrap icons
 
 **Wire Frames/Hi-Rez Mock ups:**
 
 - [See Project on Figma](https://www.figma.com/file/oYakbnxT72r856QIczCCF7/Untitled?node-id=13%3A15)
 
 
-### Setup
+### Local Host on Mac
 
+- $git clone https://github.com/LWRGitHub/lover-artist-site.git
+- $cd lover-artist-site
 - $pip3 install flask
 - $python3 app.py
